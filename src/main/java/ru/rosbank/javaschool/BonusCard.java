16f50b05aacc1f071780a118234d341c5ae8b2fb
AndreyLev <balashovdon@gmail.com>
2019-11-18 15:51:49 +0300

    fixed #4

--- src/main/java/ru/rosbank/javaschool/BonusCard.java
+++ src/main/java/ru/rosbank/javaschool/BonusCard.java
@@ -2,16 +2,16 @@ package ru.rosbank.javaschool;
 
 public class BonusCard {
 
-    public final static int blueCardMinAmount = 1;
-    public final static int silverCardMinAmount = 15001;
-    public final static int goldCardMinAmount = 151000;
+    public final static int BLUE_CARD_MIN_AMOUNT = 1;
+    public final static int SILVER_CARD_MIN_AMOUNT = 15001;
+    public final static int GOLD_CARD_MIN_AMOUNT = 151000;
 
-    public final static int blueCardBonusValue = 50;
-    public final static int silverCardBonusValue = 70;
-    public final static int goldCardBonusValue = 100;
+    public final static int BLUE_CARD_BONUS_VALUE = 50;
+    public final static int SILVER_CARD_BONUS_VALUE = 70;
+    public final static int GOLD_CARD_BONUS_VALUE = 100;
 
-    public final static int amountPerBonusUnit = 1000;
+    public final static int AMOUNT_PER_BONUS_UNIT = 1000;
 
-    public final static int zero = 0;
+    public final static int ZERO = 0;
 
 }
--- src/main/java/ru/rosbank/javaschool/service/CardBonusService.java
+++ src/main/java/ru/rosbank/javaschool/service/CardBonusService.java
@@ -5,12 +5,12 @@ import ru.rosbank.javaschool.BonusCard;
 public class CardBonusService {
 
 
-    final static int penniesInRuble = 100;
+    final static int PENNIES_IN_RUBLE = 100;
 
     public int bonusForCurrentPurchase(double accumulatedAmount, double currentPurchaseValue) {
 
-        int accumulatedAmountInPennies = (int)(accumulatedAmount * penniesInRuble);
-        int currentPurchaseValueInPennies = (int)(currentPurchaseValue * penniesInRuble);
+        int accumulatedAmountInPennies = (int)(accumulatedAmount * PENNIES_IN_RUBLE);
+        int currentPurchaseValueInPennies = (int)(currentPurchaseValue * PENNIES_IN_RUBLE);
         int bonus = 0;
 
         if (accumulatedAmountInPennies <= 0) {
@@ -21,17 +21,17 @@ public class CardBonusService {
             return 0;
         }
 
-        if (accumulatedAmountInPennies < (BonusCard.silverCardMinAmount * penniesInRuble)) {
-            bonus = BonusCard.blueCardBonusValue * (int) Math.floor((double)currentPurchaseValueInPennies / (BonusCard.amountPerBonusUnit * penniesInRuble));
+        if (accumulatedAmountInPennies < (BonusCard.SILVER_CARD_MIN_AMOUNT * PENNIES_IN_RUBLE)) {
+            bonus = BonusCard.BLUE_CARD_BONUS_VALUE * (int) Math.floor((double)currentPurchaseValueInPennies / (BonusCard.AMOUNT_PER_BONUS_UNIT * PENNIES_IN_RUBLE));
             return bonus;
         }
 
-        if (accumulatedAmountInPennies < (BonusCard.goldCardMinAmount * penniesInRuble)) {
-            bonus = BonusCard.silverCardBonusValue * (int) Math.floor((double)currentPurchaseValueInPennies / (BonusCard.amountPerBonusUnit * penniesInRuble));
+        if (accumulatedAmountInPennies < (BonusCard.GOLD_CARD_MIN_AMOUNT * PENNIES_IN_RUBLE)) {
+            bonus = BonusCard.SILVER_CARD_BONUS_VALUE * (int) Math.floor((double)currentPurchaseValueInPennies / (BonusCard.AMOUNT_PER_BONUS_UNIT * PENNIES_IN_RUBLE));
             return bonus;
         }
 
-        bonus = BonusCard.goldCardBonusValue * (int) Math.floor((double)currentPurchaseValueInPennies / (BonusCard.amountPerBonusUnit * penniesInRuble));
+        bonus = BonusCard.GOLD_CARD_BONUS_VALUE * (int) Math.floor((double)currentPurchaseValueInPennies / (BonusCard.AMOUNT_PER_BONUS_UNIT * PENNIES_IN_RUBLE));
         return bonus;
     }
 
